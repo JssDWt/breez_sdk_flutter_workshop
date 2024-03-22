a07bfc36e8d5735a831b84b5bfe856d5cd83b04d
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-03-22 16:44:38 +0300

    Address linter warnings

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ final sdk = BreezSDK();
 void main() async {
   Logger.root.level = Level.ALL; // defaults to Level.INFO
   Logger.root.onRecord.listen((record) {
+    // ignore: avoid_print
     print('${record.level.name}: ${record.time}: ${record.message}');
   });
 
