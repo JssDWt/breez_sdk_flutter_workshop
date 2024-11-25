0e6c1a830cd08589cb81350f75555fd80366d202
Jesse de Wit <witdejesse@hotmail.com>
2024-11-25 21:45:25 +0100

    send a payment

--- lib/send_dialog.dart
+++ lib/send_dialog.dart
@@ -1,4 +1,7 @@
+import 'package:breez_sdk/bridge_generated.dart';
 import 'package:flutter/material.dart';
+
+import 'main.dart';
 
 class SendPaymentDialog extends StatefulWidget {
   const SendPaymentDialog({super.key});
@@ -27,6 +30,15 @@ class _SendPaymentDialogState extends State<SendPaymentDialog> {
         TextButton(
           onPressed: () {
             setState(() => _payInProgress = true);
+            sdk
+                .sendPayment(
+                  req: SendPaymentRequest(
+                    bolt11: invoiceController.text,
+                    useTrampoline: true,
+                  ),
+                )
+                .then((_) => Navigator.of(context).pop())
+                .onError((error, stackTrace) => debugPrint("ERROR in sendPayment: $error"));
           },
           child: const Text("OK"),
         ),
